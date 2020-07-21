95386dcf29a64b52cdf9461dc2cfaae32f014a2b
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-07-21 15:50:48 +0200

    Add relation OneToMany between Poi and Comment entities

--- src/Entity/Poi.php
+++ src/Entity/Poi.php
@@ -89,12 +89,18 @@ class Poi
      */
     private $offers;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="poi")
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->categories = new ArrayCollection();
         $this->tags = new ArrayCollection();
         $this->reviews = new ArrayCollection();
         $this->offers = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -331,6 +337,37 @@ class Poi
             // set the owning side to null (unless already changed)
             if ($offer->getPoi() === $this) {
                 $offer->setPoi(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setPoi($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->contains($comment)) {
+            $this->comments->removeElement($comment);
+            // set the owning side to null (unless already changed)
+            if ($comment->getPoi() === $this) {
+                $comment->setPoi(null);
             }
         }
 
