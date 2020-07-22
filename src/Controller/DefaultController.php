4247bbaf7fe6b7db987cb83e4ac3c981a596c447
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-07-22 14:49:18 +0200

    Create nested research of Pois : by area and by department. Create templates base and default

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,26 +2,84 @@
 
 namespace App\Controller;
 
+use App\Entity\Poi;
+use App\Form\DefaultType;
+use App\Repository\PoiRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route(name="pois_")
+ * @Route(name="default_")
  */
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="list")
+     * List the Pois according to their localization
+     * 
+     * @Route("/" , name="list")
+     * @param PoiRepository $poiRepository
+     * @param Request $request
+     * @return Response
      */
-    public function index()
+    public function list(Request $request, PoiRepository $repository): Response
     {
-        return new \Symfony\Component\HttpFoundation\Response(<<<EOF
-<html>
-    <body>
-        <img src="/images/under-construction.gif" />
-    </body>
-</html>
-EOF
-    );
+        // Create a new object Poi and a DefaultType form associated that we fill with the request
+        $pois = new poi();
+        $form = $this->createForm(DefaultType::class, $pois);
+        $form->handleRequest($request);
+
+        // if the form is submitted and validated : send the datas
+        if ($form->isSubmitted() && $form->isValid()) {
+            // If area AND department are null : return the list of all Poi in France
+            if ($request->request->get('default')['area'] == null) {
+                  if($request->request->get('default')['department'] == null) {
+                    // Get the method in the repository
+                    $pois = $repository->findAll();
+
+                    // Return all pois
+                    return $this->render('default/list.html.twig', [
+                        'form' => $form->createView(),
+                        'pois' => $pois
+                    ]);
+                  }
+
+            // IF department is not null : return the list of Poi which match with department
+            } elseif ($request->request->get('default')['department'] != null) {
+                // Get and save department id
+                $id = $request->request->get('default')['department'];
+                // Get the method in the repository
+                $pois = $repository->findAllByDepartment($id);
+
+                return $this->render('default/list.html.twig', [
+                    'form' => $form->createView(),
+                    'pois' => $pois
+                ]);
+
+            // If area is null AND the department is not null : return the list of Poi which match with area
+            } else {
+                    // Get and save area id
+                    $id = $request->request->get('default')['area'];
+                    dump($id);
+                    // Get the method in the repository
+                    $pois = $repository->findAllByArea($id);
+
+                    return $this->render('default/list.html.twig', [
+                        'form' => $form->createView(),
+                        'pois' => $pois
+                    ]);
+
+
+            }
+
+        }
+
+        return $this->render('default/list.html.twig', [
+            'form' => $form->createView(),
+            'pois' => $pois
+
+        ]);
     }
+
 }
