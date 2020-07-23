f9fa289ea14fb06e93839214313a4b6668cd42ab
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-07-23 11:01:12 +0200

    Symfony Webpack Encore : Install and configure node-sass+sass-loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,8 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-// any CSS you require will output into a single css file (app.css in this case)
-import '../css/app.css';
+// any SCSS you require will output into a single css file (app.scss in this case)
+import '../css/app.scss';
 
 /**
  * JQUERY
