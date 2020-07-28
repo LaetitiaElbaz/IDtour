20767db7becb962c98336266bbee534a4767469f
LaetitiaElbaz <laetitiaelbaz.web@gmail.com>
2020-07-28 15:17:26 +0200

    Make register impossible when email already exists in database

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,15 +2,20 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @UniqueEntity(
+ * fields={"email"},
+ * message="L'email que vous indiquez est déjà utilisé."
+ * )
  */
 class User implements UserInterface
 {
